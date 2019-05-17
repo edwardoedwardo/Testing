fc59d79128598daeef2848c12ec60464263ef903
Larbi <Larbi@DESKTOP-CADGQ8M.mshome.net>
2019-05-17 02:16:59 +0100

    First Commit

---
+++ TestProject/src/Test.java
@@ -0,0 +1,20 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+	  String s1=new String();
+	  String s2=new String("HiGitEasy");
+	  String s3="";
+	  
+	  String ss=s2.substring(1, 5);
+	  
+	 
+	  
+	  System.out.println(s2.charAt(0));
+	  System.out.println(s2.length());
+	  System.out.println(s2);
+
+
+	}
+
+}
